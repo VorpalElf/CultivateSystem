3f154028e74d177e3fb0d9fd744526ac54ad0c19
VorpalElf <jeremy20131085@gmail.com>
2025-06-25 15:54:49 +0100

    Initial Commit

---
+++ Cultivate/Cultivate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cultivate
+//
+//  Created by Jeremy Lo on 25/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Cultivate/Cultivate/CultivateApp.swift
@@ -0,0 +1,17 @@
+//
+//  CultivateApp.swift
+//  Cultivate
+//
+//  Created by Jeremy Lo on 25/6/25.
+//
+
+import SwiftUI
+
+@main
+struct CultivateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
